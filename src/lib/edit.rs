10737ea2613153d31e5e44645d9588b68ead9370
Javier Iserte <jiserte@leloir.org.ar>
2020-09-28 08:47:17 -0300

    [#3] WIP: Edit sequences

--- src/lib/edit.rs
+++ src/lib/edit.rs
@@ -33,7 +33,7 @@ impl EditSequence for AnnotatedSequence {
     ) -> Result<(), SeqError> {
         match self.seq_mut() {
             Some(x) => {
-                if at < x.len()  {
+                if at <= x.len()  {
                     x.splice(at..at, new);
                     Ok(())
                 } else {
@@ -82,8 +82,55 @@ impl EditSequence for AnnotatedSequence {
 }
 
 mod test {
+    use super::*;
     #[test]
-    fn name() {
-        unimplemented!();
+    fn insert_one_char_at_begging() {
+        let mut s1 = AnnotatedSequence::from_string(
+            String::from("seq1"),
+            String::from("ACTG")
+        );
+        s1.edit_insert(vec!['X'], 0).unwrap();
+        assert_eq!(
+            s1.seq_as_string(), String::from("XACTG"));
     }
+
+    #[test]
+    fn insert_one_char_at_middle() {
+        let mut s1 = AnnotatedSequence::from_string(
+            String::from("seq1"),
+            String::from("ACTG")
+        );
+        s1.edit_insert(vec!['X'], 2).unwrap();
+
+        assert_eq!(
+            s1.seq_as_string(), String::from("ACXTG"));
+    }
+    
+    #[test]
+    fn insert_one_char_at_end() {
+        let mut s1 = AnnotatedSequence::from_string(
+            String::from("seq1"),
+            String::from("ACTG")
+        );
+        s1.edit_insert(vec!['X'], 4).unwrap();
+        
+        assert_eq!(
+            s1.seq_as_string(), String::from("ACTGX"));
+    }
+    
+    #[test]
+    fn insert_one_char_at_after_end() {
+        let mut s1 = AnnotatedSequence::from_string(
+            String::from("seq1"),
+            String::from("ACTG")
+        );
+        match s1.edit_insert(vec!['X'], 5) {
+            Ok(()) => panic!("This should throw SeqError::EditError"),
+            Err(x) => match x {
+                SeqError::EditError => {},
+                _ => panic!("This should throw SeqError::EditError")
+            }
+        }
+    }
+
 }
