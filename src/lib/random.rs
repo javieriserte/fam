a7e1d7fb21ed91da6b8d8adacdf3831ae33170a2
Javier Iserte <jiserte@leloir.org.ar>
2021-02-02 11:06:50 -0300

    [#10] WIP: Random msa

--- src/lib/random.rs
+++ src/lib/random.rs
@@ -1,8 +1,10 @@
-use crate::seqs::{Alignment, SequenceAccesors};
+use crate::seqs::{Alignment, SeqError, SequenceAccesors};
 extern crate rand;
 use rand::{prelude::SliceRandom, thread_rng};
 pub trait RandomGen {
     fn shuffle(&mut self, fixed_gaps: bool);
+    fn shuffle_rows(&mut self);
+    fn shuffle_cols(&mut self);
 }
 
 impl RandomGen for Alignment {
@@ -28,8 +30,42 @@ impl RandomGen for Alignment {
                     }
                 }
             },
-            false=> {}
+            false=> {
+                for i in 0..self.size() {
+                    let seq = self.get_mut(i).unwrap().seq_mut().unwrap();
+                    let slice: &mut [char] = seq;
+                    slice.shuffle(&mut rng);
+                }
+            }
         };
+    }
+
+    fn shuffle_rows(&mut self) {
+        let mut rng = thread_rng();
+        let mut rowdata = vec![];
+        for i in 0..self.size() {
+            rowdata.push(self.seqs.remove(i).unwrap());
+        }
+        let slice: &mut [_] = &mut rowdata;
+        slice.shuffle(&mut rng);
+        for i in 0..slice.len() {
+            self.seqs.add(slice[i].clone()).unwrap();
+        }
+    }
+
+    fn shuffle_cols(&mut self) {
+        let mut rng = thread_rng();
+        let mut col_index: Vec<_> = (0..self.length()).into_iter().collect();
+        let slice: &mut [_] = &mut col_index;
+        slice.shuffle(&mut rng);
+        for (old, new) in (0..self.length()).into_iter().zip(slice.iter()) {
+            for i in 0..self.size() {
+                let c_seq = self.get_mut(i).unwrap().seq_mut().unwrap();
+                let tmp = c_seq[old];
+                c_seq[old] = c_seq[*new];
+                c_seq[*new] = tmp;
+            }
+        }
     }
 }
 
@@ -59,7 +95,7 @@ mod test {
         msa
     }
     #[test]
-    fn test_random_msa() {
+    fn test_random_msa_with_fixed_gaps() {
         let mut msa=sample_msa();
         let prev = msa.get(0).unwrap().clone().seq_as_string();
         let gapped_prev = prev
@@ -68,10 +104,7 @@ mod test {
             .filter_map(
                 |(i, c)| if c=='-' { None } else {Some(i)})
             .collect::<Vec<_>>();
-
-
         msa.shuffle(true);
-
         let post = msa.get(0).unwrap().clone().seq_as_string();
         let gapped_post = post
             .chars()
@@ -81,7 +114,27 @@ mod test {
             .collect::<Vec<_>>();
         assert_ne!(prev, post);
         assert_eq!(gapped_prev, gapped_post);
-
+    }
+    #[test]
+    fn test_random_msa_with_no_fixed_gaps() {
+        let mut msa=sample_msa();
+        let prev = msa.get(0).unwrap().clone().seq_as_string();
+        let gapped_prev = prev
+            .chars()
+            .enumerate()
+            .filter_map(
+                |(i, c)| if c=='-' { None } else {Some(i)})
+            .collect::<Vec<_>>();
+        msa.shuffle(false);
+        let post = msa.get(0).unwrap().clone().seq_as_string();
+        let gapped_post = post
+            .chars()
+            .enumerate()
+            .filter_map(
+                |(i, c)| if c=='-' { None } else {Some(i)})
+            .collect::<Vec<_>>();
+        assert_ne!(prev, post);
+        assert_ne!(gapped_prev, gapped_post);
     }
 }
 
